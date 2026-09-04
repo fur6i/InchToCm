18e9d07644662793220224ce163756043a17d486
furbi <dev@furbi.lol>
2026-09-04 21:39:16 +0200

    very first c++ commit

---
+++ InchToCm.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	double inches;
+
+	std::cout << "Insert the value in inches: ";
+	std::cin >> inches;
+
+	double centimeters = inches * 2.54;
+
+	std::cout << inches << " inches are " << centimeters << " centimeters.";
+}
+
+	// as basic as a program can get, 1st project outside of codedex. (started learning c++ literally 2h ago lol)
